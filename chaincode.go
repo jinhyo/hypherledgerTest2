c37f62a2073481144cbd7c345c536c27cb078d33
sosilion <sosilion@gmail.com>
2020-02-23 18:22:12 +0900

    2. func Init()

--- chaincode.go
+++ chaincode.go
@@ -5,7 +5,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	sc "github.com/hyperledger/fabric/protos/peer"
@@ -15,10 +17,61 @@ import (
 type ERC20Chaincode struct {
 }
 
+// ERC20Metadata is the definition of the Token meta info
+type ERC20Metadata struct {
+	Name        string `json: "name"`
+	Symbol      string `json: "symbol"`
+	Owner       string `json: "owner"`
+	TotalSupply uint64 `json: "totalsupply`
+}
+
 // Init is called when the chaincode is instantiated by the blockchain network.
+// params : tokenName, symbol, owner(address), amount
 func (cc *ERC20Chaincode) Init(stub shim.ChaincodeStubInterface) sc.Response {
-	fcn, params := stub.GetFunctionAndParameters()
-	fmt.Println("Init()", fcn, params)
+	_, params := stub.GetFunctionAndParameters()
+	fmt.Println("Init is called with params:", params)
+	if len(params) != 4 {
+		return shim.Error("incorrect number of the params")
+	}
+
+	tokenName, symbol, owner, amount := params[0], params[1], params[2], params[3]
+
+	// check amount is unsigned int
+	amountUint, err := strconv.ParseUint(amount, 10, 64)
+	if err != nil {
+		return shim.Error("amount must be a number or cannot be negative")
+	}
+
+	// tokenName, symbol, owner cannot be empty
+	if len(tokenName) == 0 || len(symbol) == 0 || len(owner) == 0 {
+		return shim.Error("tokenName, symbol, owner cannont be empty")
+	}
+
+	// make meta data
+	erc20 := ERC20Metadata{
+		Name:        tokenName,
+		Symbol:      symbol,
+		Owner:       owner,
+		TotalSupply: amountUint}
+
+	erc20Bytes, err := json.Marshal(erc20)
+	if err != nil {
+		return shim.Error("failed to Marshal erc20, error: " + err.Error())
+	}
+
+	// save token to database
+	err = stub.PutState(tokenName, erc20Bytes)
+	if err != nil {
+		return shim.Error("failed to PutState erc20, error: " + err.Error())
+	}
+
+	// save owner balance
+	err = stub.PutState(owner, []byte(amount))
+	if err != nil {
+		return shim.Error("failed to PutState erc20, error: " + err.Error())
+	}
+
+	// response
 	return shim.Success(nil)
 }
 
